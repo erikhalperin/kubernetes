6a435552a13bab666999e3bd1fcb67b1680a8fa2
Erik Halperin <ehalperin@hubspot.com>
2026-01-15 12:27:27 -0500

    remove unnecessary code

--- staging/src/k8s.io/apiserver/pkg/storage/cacher/cacher.go
+++ staging/src/k8s.io/apiserver/pkg/storage/cacher/cacher.go
@@ -1019,9 +1019,6 @@ func (c *Cacher) dispatchEvent(event *watchCacheEvent) {
 			// is running, not only the first ones in the list.
 			timer := c.timer
 			for _, watcher := range c.blockedWatchers {
-				if !watcher.initEventDone {
-					continue
-				}
 				if !watcher.add(event, timer) {
 					// fired, clean the timer by set it to nil.
 					timer = nil
