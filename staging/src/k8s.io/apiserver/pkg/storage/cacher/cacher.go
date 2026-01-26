49c560fbbdd30ca543f9bbbdaed9c75379d2baca
Erik Halperin <ehalperin@hubspot.com>
2026-01-26 12:37:40 -0500

    increase max event time

--- staging/src/k8s.io/apiserver/pkg/storage/cacher/cacher.go
+++ staging/src/k8s.io/apiserver/pkg/storage/cacher/cacher.go
@@ -622,8 +622,8 @@ func (c *Cacher) Watch(ctx context.Context, key string, opts storage.ListOptions
 	if isListWatchRequest(opts) {
 		initEventCount := cacheInterval.buffer.endIndex
 		klog.V(1).Infof("initEvent count: %d", initEventCount)
-		// The slice is of pointers, each pointer is 8 bytes, so the starting max size is 80KB
-		bufferSize := min(10000, initEventCount*4)
+		// The slice is of pointers, each pointer is 8 bytes, so the starting max size is 800KB
+		bufferSize := min(100000, initEventCount/2)
 		watcher.waitInitEventTemporary = make([]*watchCacheEvent, 0, bufferSize)
 	}
 
